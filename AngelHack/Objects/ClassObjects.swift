4878b495edad2eb0a69cb2bccd6cea99e62a12c9
Jamal <16698402+trujamal@users.noreply.github.com>
2018-07-12 20:10:22 -0500

    Added TableView Classes To Application

---
+++ AngelHack/Objects/ClassObjects.swift
@@ -0,0 +1,18 @@
+//
+//  ClassObjects.swift
+//  AngelHack
+//
+//  Created by Jamal Rasool on 7/12/18.
+//  Copyright © 2018 Jamal Rasool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DietPlans: NSObject {
+    
+    var tableViewText: String?
+    var date: NSNumber?
+    var userID: String?
+    
+}
